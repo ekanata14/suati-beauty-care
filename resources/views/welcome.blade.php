f960be751700d7a3d8028715b2320f0a8c72a8ab
ekanata14 <ekanata1411@gmail.com>
2025-06-16 21:15:03 +0800

    feat: progress revision

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     @foreach($homeContents as $content)
-        <section class="h-[70vh] flex flex-col md:flex-row justify-center items-center">
-            <div class="container mx-auto px-4 py-8 flex justify-center">
-                <img src="{{ asset($content['logo']) }}" alt="logo">
-            </div>
+        <section class="h-[70vh] flex flex-col md:flex-row justify-center items-center bg-white">
             <div class="container mx-auto px-4 py-8">
                 <h1 class="text-3xl font-bold mb-4">{{ $content['title'] }}</h1>
                 <p class="text-gray-700 mb-6">{{ $content['description'] }}</p>
+            </div>
+            <div class="container mx-auto px-4 py-8 flex justify-center">
+                <img src="{{ asset('assets/images/model.jpeg') }}" alt="model">
             </div>
         </section>
     @endforeach
