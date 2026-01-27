b7c33250e2e0adb2d1058fda1295d73dea953f76
ekanata14 <ekanata1411@gmail.com>
2026-01-27 19:54:19 +0800

    fix: nomor resi pengiriman placeholder

--- resources/views/admin/transaction/index.blade.php
+++ resources/views/admin/transaction/index.blade.php
@@ -290,7 +290,7 @@
                             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nomor Resi</label>
                             <input type="text" name="no_resi" id="modal_no_resi"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                placeholder="Contoh: REG / YES">
+                                placeholder="Nomor Resi Pengiriman">
                         </div>
 
                         {{-- Bukti Pembayaran / Resi Foto --}}
